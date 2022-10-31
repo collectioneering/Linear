e22085835fd15877c2e8fc09d9dd4ab506eb5bf7
v69ar73aqvc <54872398+v69ar73aqvc@users.noreply.github.com>
2022-10-30 21:47:16 -0700

    adjust tests

---
+++ src/Linear.Test/ArrayTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Linear.Test;
+
+public class ArrayTests : TestsBase
+{
+    [Test]
+    public void Array_Redirected_Correct()
+    {
+        AddBufferMethod("getbuf", new byte[]
+        {
+            0x01, 0x00, 0x00, 0x00, //
+            0x02, 0x00, 0x00, 0x00, //
+            0x03, 0x00, 0x00, 0x00, //
+            0x04, 0x00, 0x00, 0x00, //
+        });
+        var res = Create(
+            """
+main {
+    int[4] arr getbuf()!0;
+}
+"""
+        ).Parse("main", ReadOnlySpan<byte>.Empty);
+        Assert.That(res["arr"], Is.EqualTo(new int[] { 0x01, 0x02, 0x03, 0x04 }));
+    }
+
+    [Test]
+    public void PointerArray_BaseRedirected_Correct()
+    {
+        AddBufferMethod("getbuf", new byte[]
+        {
+            0x04, 0x00, 0x00, 0x00, //
+            0x08, 0x00, 0x00, 0x00, //
+            0x0C, 0x00, 0x00, 0x00, //
+            0x10, 0x00, 0x00, 0x00, //
+        });
+        var res = Create(
+            """
+main {
+    int[4] -> int[] arr getbuf()!0,0;
+}
+"""
+        ).Parse("main", new byte[]
+        {
+            0x05, 0x00, 0x00, 0x00, //
+            0x06, 0x00, 0x00, 0x00, //
+            0x07, 0x00, 0x00, 0x00, //
+            0x08, 0x00, 0x00, 0x00, //
+            0x09, 0x00, 0x00, 0x00, //
+        });
+        Assert.That(res["arr"], Is.EqualTo(new int[] { 0x06, 0x07, 0x08, 0x09 }));
+    }
+}
---
+++ src/Linear.Test/TestsBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Linear.Runtime;
+using Linear.Runtime.Expressions;
+using NUnit.Framework;
+
+namespace Linear.Test;
+
+public class TestsBase
+{
+    public Dictionary<string, DeserializerDefinition> Deserializers;
+    public Dictionary<string, MethodCallDelegate> Methods;
+
+    [SetUp]
+    public void SetUp()
+    {
+        Deserializers = new Dictionary<string, DeserializerDefinition>();
+        Methods = new Dictionary<string, MethodCallDelegate>();
+    }
+
+    public FormatStore Create(params string[] files)
+    {
+        var res = new FormatStore(Deserializers, Methods);
+        foreach (string file in files)
+            res.Add(file);
+        return res;
+    }
+
+    public void AddBufferMethod(string name, byte[] buffer)
+    {
+        Methods.Add(name, (_, _) => buffer);
+    }
+}
